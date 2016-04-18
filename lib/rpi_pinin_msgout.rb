31d7e6520d9ea03dd428b1d338765ac4e4617a48
Your Name <you@example.com>
2016-04-18 21:30:04 +0100

    1st commit

---
+++ lib/rpi_pinin_msgout.rb
@@ -0,0 +1,107 @@
+#!/usr/bin/env ruby
+
+# file: rpi_pinin_msgout.rb
+
+require 'rpi_pinin'
+require 'chronic_duration'
+
+
+class Echo
+
+  def notice(s)
+    puts "%s: %s" % [Time.now, s]
+  end
+end
+
+class RPiPinInMsgOut < RPiPinIn
+
+
+  def initialize(id, pull: nil, mode: :default, verbose: true, 
+                 subtopic: 'sensor', device_id: 'pi', notifier: Echo.new, 
+                 duration: '5 seconds', index: 0, capture_rate: 0.5)
+    
+    super(id, pull: pull)
+        
+    @mode, @verbose, @notifier, @duration = mode, verbose, notifier, duration
+    @capture_rate = capture_rate
+    @topic = [device_id, subtopic, index].join('/')
+    
+  end
+
+  def capture_high()
+   
+    case @mode
+    when :interval
+      
+      count = 0
+      
+      duration = ChronicDuration.parse(@duration)
+      
+      t1 = Time.now - duration + 1      
+      
+      setup do 
+        count, t1 = interval_mode(t1, count, duration) 
+      end
+      
+    when :default
+      setup { default_mode() }
+    end
+    
+  end
+  
+  private
+  
+  def setup()
+    
+    t0 = Time.now + 1        
+    
+    self.watch_high do 
+      
+      # ignore any movements that happened a short time ago e.g. 250 
+      #               milliseconds ago since the last movement
+      if t0 + @capture_rate < Time.now then     
+        
+        puts  Time.now.to_s if @verbose
+        
+        yield() 
+        
+        t0 = Time.now
+        
+      else
+        #puts 'ignoring ...'
+      end   
+      
+    end # /watch_high
+    
+  end    
+  
+  def interval_mode(t1, count, duration)
+                  
+    count += 1
+    
+    elapsed = Time.now - (t1  + duration)
+
+    if elapsed > 0 then
+
+      # identify if the movement is consecutive
+      msg = if elapsed < duration then              
+        s = ChronicDuration.output(duration, :format => :long)
+        "%s: detected %s times within the past %s" % [@topic, count, s ]
+      else              
+        "%s: detected" % [@topic, @index]
+      end
+      
+      @notifier.notice msg
+      count, t1 = 0, Time.now
+
+    end
+    
+    [count, t1]
+        
+  end    
+  
+  def default_mode()
+    @notifier.notice "%s: detected" % [@topic, @index]
+  end
+    
+end
---
+++ rpi_pinin_msgout.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name = 'rpi_pinin_msgout'
+  s.version = '0.1.0'
+  s.summary = 'Returns meaningful messages for humans from the capture of a Raspberry PI GPIO input pin.'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/rpi_pinin_msgout.rb']
+  s.add_runtime_dependency('rpi_pinin', '~> 0.1', '>=0.1.2')
+  s.signing_key = '../privatekeys/rpi_pinin_msgout.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@r0bertson.co.uk'
+  s.homepage = 'https://github.com/jrobertson/rpi_pinin_msgout'
+end
